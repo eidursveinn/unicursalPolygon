85e8f9b71b7d67629d2ccb781dc7c85002aa0952
kallith <karlt13@ru.is>
2016-01-24 16:18:43 +0000

    testing improved generator

--- unicursalpolygon.py
+++ unicursalpolygon.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from generator import CyclicPermutationsHalfAvoidingNeighbours
+from generator import UniqueCyclicPermutationsHalfAvoidingNeighbours
 from sympy import *
 import sys
 import os.path
@@ -209,7 +210,8 @@ def generate_stars(n, optimization=True, progress_bar=False):
     stars = []
     errors = []
     polygons = []
-    gen = CyclicPermutationsHalfAvoidingNeighbours(n, optimization=optimization)
+    #gen = CyclicPermutationsHalfAvoidingNeighbours(n, optimization=optimization)
+    gen = UniqueCyclicPermutationsHalfAvoidingNeighbours(n)
     if progress_bar:
         import time
         start_time = time.time()
