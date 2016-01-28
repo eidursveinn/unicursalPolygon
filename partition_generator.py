594b301513ef028383720c1771210456d425d75f
kallith <karlt13@ru.is>
2016-01-28 17:12:25 +0000

    total ordering and uniqueness fixed

--- partition_generator.py
+++ partition_generator.py
@@ -4,7 +4,7 @@ import sys
 def __to_standard__(part):
     n = len(part)
     def complement(lis):
-        return [n - i for i in part]
+        return [n - i for i in lis]
     res = []
     to_check = [
             part * 2,
@@ -25,8 +25,6 @@ class CyclicPartition(object):
     def __init__(self, partition):
         self.partition = __to_standard__(partition)
         self.n = len(partition)
-    def __eq__(self,other):
-        return self.partition == other.partition
     def __str__(self):
         return str(self.partition)
     def __repr__(self):
@@ -36,6 +34,12 @@ class CyclicPartition(object):
         for i,val in enumerate(reversed(self.partition)):
             res += val*self.n**i
         return res
+    def __eq__(self,other):
+        return self.partition == other.partition
+    def __lt__(self, other):
+        return self.__hash__() < other.__hash__()
+    def __ge__(self, other):
+        return not self < other
     def as_cyclic_permutation(self):
         lis = [0] * self.n
         tmp = 0
