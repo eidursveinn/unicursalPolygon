2ba7bff34e82d1f8a3ddc5974c9195c95b25b3e2
kallith <karlt13@ru.is>
2016-02-14 17:34:08 +0000

    s' function added

--- semistar_test.py
+++ semistar_test.py
@@ -24,12 +24,49 @@ def test_semistar_property(perm):
     neigh = [{perm[(i-1) % n], perm[(i+1) % n]} for i in range(n)]
     return all(semistar_property(i, perm, inv, neigh, n) for i in range(n))
 
-while 1:
-    try:
-        perm = get_csv_line()
-    except:
-        break
-    if test_semistar_property(perm):
-        print("yes", perm)
-    else:
-        print("no", perm)
+def star_property(k, perm, inv, neigh, n):
+    i = inv[k]
+    j = inv[(k+1)%n]
+    val = lambda x : (x - perm[i]) % n
+    q = val(max(neigh[i], key=val))
+    p = val(min(neigh[j], key=val))
+    r_lis = [val(r) for r in perm if p < val(r) and val(r) < n]
+    s_lis = [val(s) for s in perm if 1 < val(s) and val(s) < q]
+    rs_lines = []
+    return "what"
+
+def triple_intersection(n,p,q,r):
+    from sympy import acos,sin,cos,pi,sqrt,simplify
+    def sub(v1,v2):
+        return simplify(v1[0]-v2[0]), simplify(v1[1]-v2[1])
+    def add(v1,v2):
+        return simplify(v1[0]+v2[0]), simplify(v1[1]+v2[1])
+    def dot(v1,v2):
+        return simplify(v1[0]*v2[0] + v1[1]*v2[1])
+    def scale(k,v):
+        return simplify(k*v[0]), simplify(k*v[1])
+    def length(v):
+        return simplify(sqrt(v[0]**2 + v[1]**2))
+    magnitude = simplify(sin((n-p)*pi/n) * 2 * sin(2*pi/n/2) / sin((p-q+1) * pi/n))
+    unit_v = (simplify(-sin(q * pi / n)), simplify(cos(q * pi /n)))
+    print(unit_v)
+    intersection = add((1,0),scale(magnitude,unit_v))
+    print(intersection[0], intersection[1])
+    c0 = sub((1,0),intersection)
+    print(c0[0], c0[1])
+    cr = sub((cos(r*2*pi/n), sin(r*2*pi/n)), intersection)
+    print(cr[0], cr[1])
+    angle = simplify(acos(dot(c0,cr)/(length(c0)*length(cr))))
+    print(angle)
+    return simplify(n - r + q - angle*n/pi)
+
+if __name__ == "__main__":
+    while 1:
+        try:
+            perm = get_csv_line()
+        except:
+            break
+        if test_semistar_property(perm):
+            print("yes", perm)
+        else:
+            print("no", perm)
