6812cacb284fd61b54a5bda80dd08679c225309e
kallith <karlt13@ru.is>
2016-02-22 11:53:06 +0000

    finished script that prints graph of stars by removing something from the permutation

--- unicursalpolygon_graph.py
+++ unicursalpolygon_graph.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 from unicursalpolygon.models.unicursalpolygon import UnicursalPolygon
 from unicursalpolygon.models.cyclicpartition import CyclicPartition
-import remove_point
+from unicursalpolygon.utils import permutation_to_standard_0, perm_to_partition
+import semistar_test
 
 graph = {}
 
@@ -8,32 +10,42 @@ def add_node(perm):
     n = len(perm)
     if n < 4:
         return None
-    polygon = UnicursalPolygon(perm)
-    partition = CyclicPartition(polygon.skip_list)
-    try:
-        star = polygon.is_star()
-    except:
-        star = None
+    partition = perm_to_partition(perm)
+    star = semistar_test.classify_permutation(perm)
     if not partition in graph:
+        # adjacent permutations
         d = {}
         for i in range(len(perm)):
             tmp = list(perm)
             x = tmp.pop(i)
-            to_check = remove_point.standardize(tmp)
+            to_check = permutation_to_standard_0(tmp)
             child = add_node(to_check)
             if child is not None:
-                d[x] = child
+                if child not in d:
+                    d[child] = []
+                d[child].append(x)
         graph[partition] = (star, d)
     return partition
 
 
+n = 0
 #also this
 while 1:
     try:
         perm = [int(i) for i in input().split(',')]
     except EOFError:
         break
+    n = max(n, len(perm))
     add_node(perm)
 
+
+def print_recursive(node, removed, starclass, adjacent, indent=0):
+    space = " " * (indent * 4)
+    print(space, starclass, node.as_cyclic_permutation(), removed)
+    for key in adjacent:
+        part = key
+        print_recursive(part, adjacent[key], graph[part][0], graph[part][1], indent=indent+1)
+
 for key in graph:
-    print(key,graph[key])
+    if key.n == n:
+        print_recursive(key, [], graph[key][0], graph[key][1])
